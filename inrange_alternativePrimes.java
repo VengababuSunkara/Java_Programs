4a3e8be25c899bedd920813c6483ba354cc62bed
VengababuSunkara <sunkaravengababu0707@gmail.com>
2025-08-10 12:24:04 +0530

    Create inrange_alternativePrimes.java
    input: 5 19
    output: 7 13 19

---
+++ inrange_alternativePrimes.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Main
+{
+    static void range(int a ,int b)
+    {
+        int c=0;
+        for(int i=a;i<=b;i++)
+        {
+            if(isPrime(i))
+            {
+                c++;
+                if(c%2==0)
+                {
+                    System.out.print(i+" ");
+                }
+            }
+        }
+    }
+    
+    static boolean isPrime(int n)
+    {
+        if(n<=1 || (n%2==0 &&n!=2))
+            return false;
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+	    range(a,b);
+	}
+}
